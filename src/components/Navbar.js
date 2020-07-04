181ecd219a08cfcb242d1abf005c67c9e875b286
Max0nyshchenko <max.0nyshenk03@gmail.com>
2020-07-05 02:28:22 +0300

    tablet css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../img/logo.svg";
+import burgerLogo from "../img/menu icon.svg";
 
 export default function Navbar() {
   return (
@@ -7,33 +8,39 @@ export default function Navbar() {
       <div className="logo">
         <img className="logo-img" src={logo} alt="logo" />
       </div>
-      <ul className="menu">
-        <li>
-          <a className="link" href="/">
-            About me
-          </a>
-        </li>
-        <li>
-          <a className="link" href="/">
-            Relationships
-          </a>
-        </li>
-        <li>
-          <a className="link" href="/">
-            Requirements
-          </a>
-        </li>
-        <li>
-          <a className="link" href="/">
-            Users
-          </a>
-        </li>
-        <li>
-          <a className="link" href="/">
-            Sign up
-          </a>
-        </li>
-      </ul>
+      {window.innerWidth > 768 ? (
+        <ul className="menu">
+          <li>
+            <a className="link" href="/">
+              About me
+            </a>
+          </li>
+          <li>
+            <a className="link" href="/">
+              Relationships
+            </a>
+          </li>
+          <li>
+            <a className="link" href="/">
+              Requirements
+            </a>
+          </li>
+          <li>
+            <a className="link" href="/">
+              Users
+            </a>
+          </li>
+          <li>
+            <a className="link" href="/">
+              Sign up
+            </a>
+          </li>
+        </ul>
+      ) : (
+        <button className="burger" type="button">
+          <img src={burgerLogo} alt="burger icon" />
+        </button>
+      )}
     </header>
   );
 }
